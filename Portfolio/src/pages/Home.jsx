573af48d597e2ce698efa98f3c8ce5eb29983388
AlishaKausheen <alishakausheen@gmail.com>
2024-03-16 08:18:46 -0700

    3d model started

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import Loader from '../components/Loader';
+
+{/* <div className='absolute top-20 left-0 right-0 z-10 flex items-center
+      justify-center'>
+  POPUP
+</div> */}
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-screen relative'>
+      <Canvas className='w-full h-screen bg-transparent'
+      camera={{near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader/>}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+        
+     </Canvas>
+    </section>
   )
 }
 
