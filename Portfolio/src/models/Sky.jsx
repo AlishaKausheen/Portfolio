9744de715d119a3f0737b83edd6a4a43d5cfb973
AlishaKausheen <alishakausheen@gmail.com>
2024-03-17 07:56:25 -0700

    skying motion add

--- Portfolio/src/models/Sky.jsx
+++ Portfolio/src/models/Sky.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import skyScene from '../assets/3d/sky.glb'
+import { useFrame } from '@react-three/fiber';
 
-const Sky = () => {
+const Sky = ({isRotating}) => {
     const sky = useGLTF(skyScene);
+    const skyRef = useRef();
+
+    useFrame((_,delta) => {
+        if (isRotating) {
+            skyRef.current.rotation.y += 0.25 *delta
+        }
+    })
+
     return (
-        <mesh>
+        <mesh ref={skyRef}>
             <primitive object={sky.scene}/>
       </mesh>
   )
