502263785287464806f06efed20757a8bca6faaa
AlishaKausheen <alishakausheen@gmail.com>
2024-03-17 00:20:32 -0700

    sky added

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import Loader from '../components/Loader';
 import Island  from '../models/Island';
+import Sky from '../models/Sky';
 
 {/* <div className='absolute top-20 left-0 right-0 z-10 flex items-center
       justify-center'>
@@ -30,11 +31,11 @@ const Home = () => {
       camera={{near: 0.1, far: 1000}}
       >
         <Suspense fallback={<Loader/>}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position = {[1,1,1]} intensity={2} />
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor='#b1e1ff' groundColor='#000000'
+            intensity={1} />
+          <Sky/>
           <Island
             position={isLandPosition}
             scale={islandScale}
